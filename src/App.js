192d5ca4477e995c7c49df1159131d9382442c9d
Geziel Carvalho <geziel.natal@gmail.com>
2020-12-05 11:52:33 -0300

    Passando parâmetros como objeto

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import MainContent from './components/MainContent'
+import ContactCard from './components/ContactCard'
+// import MainContent from './components/MainContent'
 
 function App() {
   return (
     <>
       <Header />
-      <MainContent/>
+      <ContactCard 
+        contact={{
+          name:"João da Silva", 
+          phone:"11 9999 6666", 
+          email:"joao@silva.net", 
+          imgUrl:"https://picsum.photos/200"
+        }}
+        />
+        
+       <ContactCard
+        contact={{
+          name:"José da Silva", 
+          phone:"11 9999 6666", 
+          email:"joao@silva.net", 
+          imgUrl:"https://picsum.photos/201"
+        }}
+        />
+      <ContactCard
+        contact={{
+          name:"Maria da Silva", 
+          phone:"11 9999 6666", 
+          email:"joao@silva.net", 
+          imgUrl:"https://picsum.photos/202"
+        }}
+        />
+      <ContactCard
+        contact={{
+          name:"Joana da Silva", 
+          phone:"11 9999 6666", 
+          email:"joao@silva.net", 
+          imgUrl:"https://picsum.photos/203"
+        }}
+        />
+      {/* <MainContent/> */}
       <Footer/>
     </>
   );
